bd63d5fd515477dc20503f9d2a74cc62dadf99b4
Kamaa <kamaajose028@gmail.com>
2023-07-05 09:18:55 +0300

    made changes to system check cpu usage

--- Home.py
+++ Home.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
-def welcome():
-    print("this is my first project in github")
+import os
+import sys
+import psutil
 
-welcome()
+def check_cpu_usage(percent):
+    usage = psutil.cpu_percent(1)
+    print("DEBUG: usage: {}".format(usage))
+    return usage < percent
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Pending Reboot in this system")
+        sys.exit(1)
+    else:
+        if not check_cpu_usage(75):
+            print("ERROR! CPU is overloaded")
+        else:
+            print("Everything is ok")
+
+main()
